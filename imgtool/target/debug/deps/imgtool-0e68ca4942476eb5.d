8cdc7bdc0b9d1f9ddb23ec7ad8acb0c4e90c9e63
mbsd <mbsd@m0x.ru>
2020-12-23 00:12:13 -0300

    rotate image

--- imgtool/target/debug/deps/imgtool-0e68ca4942476eb5.d
+++
@@ -1,5 +0,0 @@
-/home/msi/src/me/rust-study/imgtool/target/debug/deps/imgtool-0e68ca4942476eb5: src/main.rs
-
-/home/msi/src/me/rust-study/imgtool/target/debug/deps/imgtool-0e68ca4942476eb5.d: src/main.rs
-
-src/main.rs:
--- imgtool/target/debug/imgtool.d
+++
@@ -1,1 +0,0 @@
-/home/msi/src/me/rust-study/imgtool/target/debug/imgtool: /home/msi/src/me/rust-study/imgtool/src/main.rs

f1549124f4d5035e56f06b2a5a8613e23e1ac71b
mbsd <mbsd@m0x.ru>
2020-12-23 12:24:07 -0300

    integration tests

---
+++ integration_test/src/lib.rs
@@ -0,0 +1,6 @@
+// unit_test/src/lib.rs
+
+// function we want to test
+pub fn sum(a: i8, b: i8) -> i8 {
+    a + b
+}
---
+++ integration_test/tests/common.rs
@@ -0,0 +1,7 @@
+pub fn setup() {
+    println!("Setting up fixtures");
+}
+
+pub fn teardown() {
+    println!("tearing down");
+}
